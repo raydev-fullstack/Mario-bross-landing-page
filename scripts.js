da0353c2e53338999c79d6314c4633c3217005ae
raydev-fullstack <rayandracosta52@gmail.com>
2026-03-22 15:18:42 -0300

    primeiro upload do projeto

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+function cliqueiNoBotao(){
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+    
+
+}
+   
+
+function cliqueiNaMascara(){
+    mascara.style.visibility = "hidden"
+    form.style.left = "-235px"
+    form.style.transform = "translateX(0)"
+}
